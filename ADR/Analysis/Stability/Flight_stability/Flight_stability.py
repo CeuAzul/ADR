6b5b7763d9fe8bcdd47891dbc2b1feb5529c2cf9
David Steiner Sand <davidssand@hotmail.com>
2019-02-11 15:06:59 -0200

    Adapting analysis for big ADR change made by Rafael
    Plotting CM_alpha right

--- ADR/Analysis/Stability/Flight_stability/Flight_stability.py
+++ ADR/Analysis/Stability/Flight_stability/Flight_stability.py
@@ -7,56 +7,59 @@ import math
 from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
+from scipy import interpolate
+from scipy.optimize import root_scalar
 
-from ADR.Components.Planes.Aerodynamic_surfaces.Wing import Wing
-from ADR.Components.Planes.Aerodynamic_surfaces.EH import EH
-from ADR.Components.Planes.References.Static_margin import SM
+from ADR.Components.Aerodynamic_components.Wing import Wing
+from ADR.Components.Aerodynamic_components.HS import HS
+from ADR.Components.References.Static_margin import SM
 from ADR.Analysis.Stability.Flight_stability.Momentum_CG import wing_momentum
 from ADR.Analysis.Stability.Flight_stability.Momentum_CG import tail_momentum
+from ADR.Components.Points.CG import CG
 
 # --- Reescreve arquivos do X5 --- #
-'''
+"""
 # processando arquivos de dados do x5
-file1 = open("X5_files2/Asa.txt", "r")
-file2 = open("X5_files2/Asa_escrita.txt", "wt")
+file1 = open("X5_files/Asa.txt", "r")
+file2 = open("X5_files/Asa_escrita.txt", "wt")
 for line in file1:
-    file2.write(' '.join(line.split()) + '\n')
+    file2.write(" ".join(line.split()) + "\n")
 file1.close()
 file2.close()
-file1 = open("X5_files2/Profundor.txt", "r")
-file2 = open("X5_files2/Profundor_escrito.txt", "wt")
+file1 = open("X5_files/Profundor.txt", "r")
+file2 = open("X5_files/Profundor_escrito.txt", "wt")
 for line in file1:
-    file2.write(' '.join(line.split()) + '\n')
+    file2.write(" ".join(line.split()) + "\n")
 file1.close()
 file2.close()
-file1 = open("X5_files2/Aviao.txt", "r")
-file2 = open("X5_files2/Aviao_escrito.txt", "wt")
+file1 = open("X5_files/Aviao.txt", "r")
+file2 = open("X5_files/Aviao_escrito.txt", "wt")
 for line in file1:
-    file2.write(' '.join(line.split()) + '\n')
+    file2.write(" ".join(line.split()) + "\n")
 file1.close()
 file2.close()
-'''
+"""
 
 ### inputs variáveis
 # wing 1
-pd_wing = pd.read_csv('X5_files2/Asa_escrita.txt', skiprows=7, sep=' ')
-clw_wh = pd_wing.loc[:, 'CL'].values
-cdw_wh = pd_wing.loc[:, 'CD'].values
+pd_wing = pd.read_csv("X5_files/Asa_escrita.txt", skiprows=7, sep=" ")
+clw_wh = pd_wing.loc[:, "CL"].values
+cdw_wh = pd_wing.loc[:, "CD"].values
 cmw = -0.32
 
 # tail
-pd_tail = pd.read_csv('X5_files2/Profundor_escrito.txt', skiprows=7, sep=' ')
-clt_wh = pd_tail.loc[:, 'CL'].values
-cdt_wh = pd_tail.loc[:, 'CD'].values
+pd_tail = pd.read_csv("X5_files/Profundor_escrito.txt", skiprows=7, sep=" ")
+clt_wh = pd_tail.loc[:, "CL"].values
+cdt_wh = pd_tail.loc[:, "CD"].values
 cmt = 0.092
 
 # Aviao
-pd_plane = pd.read_csv('X5_files2/Aviao_escrito.txt', skiprows=7, sep=' ')
-clp_wh = pd_plane.loc[:, 'CL'].values
-cdp_wh = pd_plane.loc[:, 'CD'].values
+pd_plane = pd.read_csv("X5_files/Aviao_escrito.txt", skiprows=7, sep=" ")
+clp_wh = pd_plane.loc[:, "CL"].values
+cdp_wh = pd_plane.loc[:, "CD"].values
 
 # Angulos de ataque a serem usados
-alpha_graus_wh = pd_plane.loc[:, 'alpha'].values
+alpha_graus_wh = pd_plane.loc[:, "alpha"].values
 
 alpha_graus = []
 clw = []
@@ -68,7 +71,7 @@ cdt = []
 clp = []
 cdp = []
 
-# --- Excluindo termos com casas decimais --- #
+# --- Excl_alphauindo termos com casas decimais --- #
 for i in range(len(alpha_graus_wh)):
     if i % 2 == 0:
         alpha_graus.append(alpha_graus_wh[i])
@@ -88,187 +91,180 @@ for i in reversed(cdt_reversed):
 
 # --- Inputs --- #
 
-SM = SM()
-
-Wing1 = Wing({
-    "CL": clw, "CD": cdw, "CM": cmw,
-    "area": 0.45, "X_CA": 0.0625, "H_CA": 0, "downwash_angle": 0,
-    "chord": 0.25, "stall_min": -15, "stall_max": 15, "incidence": 0
+wing1 = Wing({
+    "x": 0,
+    "y": 0,
+    "z": 0,
+    "airfoil1": "s1223", "airfoil2": "s1223",
+    "CL_alpha": clw, "CD_alpha": cdw, "CM_alpha": cmw,
+    "span1": 1.8, "span2": 0,
+    "chord1": 0.25, "chord2": 0.25, "chord3": 0,
+    "area": 0.45,
+    "twist1": 0, "twist2": 0, "twist3": 0,
+    "X_CA": 0.0625,
+    "H_CA": 0,
+    "incidence": 0,
+    "stall_min": -20,
+    "stall_max": 20
 })
 
-Wing2 = Wing({
-    "CL": clw, "CD": cdw, "CM": cmw,
-    "area": 0.45, "X_CA": 0.0625, "H_CA": 0.7, "downwash_angle": 0,
-    "chord": 0.25, "stall_min": -15, "stall_max": 15, "incidence": 0
+wing2 = wing1
+
+hs = HS({
+    "x": 0,
+    "y": 0,
+    "z": 0,
+    "airfoil1": "s1223", "airfoil2": "s1223",
+    "CL_alpha": clt, "CD_alpha": cdt, "CM_alpha": cmt,
+    "span1": 0.47, "span2": 0,
+    "chord1": 0.2, "chord2": 0.155, "chord3": 0,
+    "area": 0.083,
+    "twist1": 0, "twist2": 0, "twist3": 0,
+    "X_CA": 0.7625,
+    "H_CA": 0.1,
+    "incidence": 0,
+    "stall_min": -20,
+    "stall_max": 20
 })
 
-EH = EH({
-    "CL": clt, "CD": cdt, "CM": cmt,
-    "area": 0.083, "X_CA": 0.7625, "H_CA": 0.1,
-    "chord": 0.1775, "stall_min": -15, "stall_max": 15, "incidence": 0
-})
+cg = CG(0.0725, 0.022)
 
 # ------ #
 
-# Variáveis internas
 
-# Posicao da origem em relacao a usada na competicao
-# 0.1528-Wing.CA.getX()
-# 0.1275
-
-Xcg_min = 0.0
-Xcg_max = 0.1
-Xcg_div = 4  # n de divisões entre min e max
-
-Hcg_min = -0.1
-Hcg_max = 0
-Hcg_div = 4
-
-# ME e alpha
-SM_min = 0.01
-SM_max = 0.2
-CM_min = -0.05
-CM_max = 0.05
-
-X_CG = 0.0725
-H_CG = 0#.02279068177
-
-# From grads to rad
 def toRad(graus):
     return [(i * math.pi / 180) for i in graus]
 
 # ------ #
 
 
-CM = []
-ME = []
-status_CM = {}
-status_ME = {}
-dCMda = []
-den = 0
-
-stall_min = max(Wing1.stall_min, Wing2.stall_min)
-stall_max = min(Wing1.stall_max, Wing2.stall_max)
-incidence_min = min(Wing1.incidence, Wing2.incidence)
-incidence_max = max(Wing1.incidence, Wing2.incidence)
-
-alpha_plane_min = stall_min - incidence_min
-alpha_plane_max = stall_max - incidence_max
-
-alpha_plane_range = range(alpha_plane_min, alpha_plane_max + 1)
+def sepDictionary(d):
+    lists = sorted(d.items())
+    x, y = zip(*lists)
+    return x, y
 
 # ------ Analise ------ #
 
 
 def flight_stability(plane_type):
 
+    alpha_wing1_range = range(wing1.stall_min, wing2.stall_max + 1)
+    alpha_tail_range = range(hs.stall_min, hs.stall_max + 1)
+
+    plane_stall_min = max(hs.stall_min, wing1.stall_min)
+    plane_stall_max = min(hs.stall_max, wing1.stall_max)
+    alpha_plane_range = range(plane_stall_min, plane_stall_max + 1)
+
+    CM_alpha_tail = {}
+    CM_alpha_wing = {}
+    CM_alpha_plane = {}
+    _SM = []
+    status_CM_alpha = {}
+    status_ME = {}
+
     print("Tipo de aviao = ", plane_type)
-    print("X_CG = ", X_CG)
-    print("H_CG = ", H_CG)
+    print("X_CG = ", cg.x)
+    print("H_CG = ", cg.h)
     print()
 
-    print("Wing1")
-    print("\tWing1.CL = ", Wing1.CL)
-    print("\tWing1.CD = ", Wing1.CD)
-    print("\tWing1.CM = ", Wing1.CM)
-    print("\tWing1.area = ", Wing1.area)
-    print("\tWing1.chord = ", Wing1.chord)
-    print("\tWing1.X_CA = ", Wing1.CA.getX())
-    print("\tWing1.H_CA = ", Wing1.CA.getH())
+    print("wing1")
+    print("\twing1.CL_alpha = ", wing1.CL_alpha)
+    print("\twing1.CD_alpha = ", wing1.CD_alpha)
+    print("\twing1.CM_alpha = ", wing1.CM_alpha)
+    print("\twing1.area = ", wing1.area)
+    print("\twing1.chord1 = ", wing1.chord1)
+    print("\twing1.X_CA = ", wing1.CA.x)
+    print("\twing1.H_CA = ", wing1.CA.h)
     print()
 
-    print("EH")
-    print("\tEH.CL = ", EH.CL)
-    print("\tEH.CD = ", EH.CD)
-    print("\tEH.CM = ", EH.CM)
-    print("\tEH.area = ", EH.area)
-    print("\tEH.chord = ", EH.chord)
-    print("\tEH.X_CA = ", EH.CA.getX())
-    print("\tEH.H_CA = ", EH.CA.getH())
+    print("hs")
+    print("\ths.CL_alpha = ", hs.CL_alpha)
+    print("\ths.CD_alpha = ", hs.CD_alpha)
+    print("\ths.CM_alpha = ", hs.CM_alpha)
+    print("\ths.area = ", hs.area)
+    print("\ths.chord1 = ", hs.chord1)
+    print("\ths.X_CA = ", hs.CA.x)
+    print("\ths.H_CA = ", hs.CA.h)
     print()
 
     print("alpha_plane_range = ", alpha_plane_range, "\n")
 
     for alpha_plane in alpha_plane_range:
+        wing1.attack_angle = wing2.attack_angle = hs.attack_angle = alpha_plane
 
-        alpha_wing1 = alpha_plane + Wing1.incidence
-        alpha_wing2 = alpha_plane + Wing2.incidence
+        # Getting CM_alpha of wing1
+        wing_m = wing_momentum(plane_type, wing1, wing2, cg, alpha_plane)
+        CM_alpha_wing[wing1.attack_angle] = wing_m
 
-        index_aw1 = alpha_wing1 + abs(Wing1.stall_min)
-        index_aw2 = alpha_wing1 + abs(Wing2.stall_min)
-        index_ap = alpha_plane + abs(alpha_plane_min)
+        # Getting CM_alpha of tail
+        tail_m = tail_momentum(wing1, hs, cg, alpha_plane)
+        CM_alpha_tail[hs.attack_angle] = tail_m
 
-        wing_m = wing_momentum(plane_type, X_CG, H_CG,
-                               Wing1.CL, Wing1.CD, Wing1.CM, index_aw1, Wing1.chord, Wing1.area, Wing1.CA.getX(), Wing1.CA.getH(),
-                               Wing2.CL, Wing2.CD, Wing2.CM, index_aw2, Wing2.chord, Wing2.area, Wing2.CA.getX(), Wing2.CA.getH(),
-                               alpha_plane)
+        # Summing CM of tail with CM of wing per each alpha
+        # Getting CM_alpha of plane
+        CM_alpha_plane[alpha_plane] = CM_alpha_wing[alpha_plane] + CM_alpha_tail[alpha_plane]
 
-        CM.append([])
-        ME.append([])
-        dCMda.append([])
+        # Calculating Static Margin for each alpha
+        sm = SM(plane_type, wing1, wing2, hs,
+                alpha_plane,
+                CM_alpha_plane[alpha_plane])
 
-        for alpha_tail in range(EH.stall_min, EH.stall_max + 1):
-            index_at = alpha_tail + abs(EH.stall_min)
+        _SM.append(sm.SM)
 
-            tail_m = tail_momentum(X_CG, H_CG,
-                                   EH.CL, EH.CD, EH.CM, index_at, EH.chord, EH.area, EH.incidence,
-                                   EH.CA.getX(), EH.CA.getH(),
-                                   Wing1.chord, Wing1.area, Wing1.downwash_angle,
-                                   alpha_plane)
+# ------- Results ------- #
 
-            CM_sum = wing_m + tail_m  # Momento total do aviao no CG
+    """print("alpha_plane_range", alpha_plane_range)
+    print("CM_alpha_wing[i]", CM_alpha_wing)
+    print("CM_alpha_tail[i]", CM_alpha_tail)
+    print("CM_alpha_plane[i]", CM_alpha_plane)
+    print()"""
 
-            # --- Verifica a qualidade de voo --- #
+    CM_alpha_plane_interp = interpolate.interp1d(alpha_plane_range, sepDictionary(CM_alpha_plane)[1])
+    CM_alpha_plane_root = root_scalar(CM_alpha_plane_interp, bracket=[plane_stall_min, plane_stall_max], method="bisect")
+    print("Plane trims for alpha(degrees) = ", CM_alpha_plane_root.root)
 
-            # Calcula a margem estatica
-            SM.set(Wing1.CL, Wing1.CD, index_aw1, Wing1.area,
-                   Wing2.CL, Wing2.CD, index_aw2, Wing2.area,
-                   EH.CL, EH.CD, index_at, EH.area, EH.incidence,
-                   Wing1.downwash_angle, 0.9, CM_sum,
-                   alpha_plane)
+    plt.figure(5)
+    plt.grid()
+    plt.title("Wing1")
+    plt.plot(alpha_wing1_range, wing1.CL_alpha, label="CL")
+    plt.plot(alpha_wing1_range, wing1.CD_alpha, label="CD")
+    plt.plot(alpha_wing1_range, wing1.dCL_dalpha, label="dCL")
+    plt.plot(alpha_wing1_range, wing1.dCD_dalpha, label="dCD")
+    plt.legend()
 
-
-            # Interpolar grafico em 2º ordem e achar zero - Moreno, 2019
-            if alpha_plane != alpha_plane_min:
-                if CM_min < CM_sum < CM_max:
-                    status_CM[alpha_plane] = alpha_tail
-
-                if SM_min < SM.get() < SM_max:
-                    status_ME[index_ap] = alpha_tail
-
-            ME[index_ap].append(SM.get())
-            CM[index_ap].append(CM_sum)
-
-
-# ------- Resultados ------- #
-# APAGUEI TEUS EIXO X DOS PLOTS
-# A TRETA TA NA INDEXAÇÃO DOS VETORES PRA CATAR OS COEFS
+    plt.figure(4)
+    plt.grid()
+    plt.title("Tail")
+    plt.plot(alpha_tail_range, hs.CL_alpha, label="CL")
+    plt.plot(alpha_tail_range, hs.CD_alpha, label="CD")
+    plt.plot(alpha_tail_range, hs.dCL_dalpha, label="dCL")
+    plt.plot(alpha_tail_range, hs.dCD_dalpha, label="dCD")
+    plt.legend()
 
     plt.figure(3)
     plt.grid()
-    plt.xlabel("Ânglo alpha do avião")
-    plt.ylabel("Coeficiente de momento no CG")
-    plt.title("Momento da aeronave para diferentes angulos de ataque e EH")
-    plt.plot(alpha_plane_range, CM)
+    plt.xlabel("Alpha angle of component")
+    plt.ylabel("CM on CG")
+    plt.title("Momentum coeficient of component on CG")
+
+
+    a = sepDictionary(CM_alpha_wing)
+    plt.plot(a[0], a[1], label="CM_alpha_wing")
+    a = sepDictionary(CM_alpha_tail)
+    plt.plot(a[0], a[1], label="CM_alpha_tail")
+    a = sepDictionary(CM_alpha_plane)
+    plt.plot(a[0], a[1], label="CM_alpha_plane")
 
     plt.figure(2)
     plt.grid()
     plt.xlabel("Ânglo alpha do avião")
     plt.ylabel("ME")
-    plt.title("ME da aeronave para diferentes angulos de ataque e EH")
-    plt.plot(alpha_plane_range, ME)
-
-    plt.figure(1)
-    a=[]
-    for i in status_CM:
-        plt.plot(i, status_CM.get(i), "o")
-    plt.grid()
-    plt.xlabel("Alpha plane")
-    plt.ylabel("Alpha EH")
-    plt.title("Pontos de trimagem")
+    plt.title("ME da aeronave para diferentes angulos de ataque e hs")
+    plt.plot(alpha_plane_range, _SM)
 
     plt.show()
 
 
 # Chama flight_stability
 flight_stability("monoplane")
+
