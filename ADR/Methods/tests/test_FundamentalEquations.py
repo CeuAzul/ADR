35e15b4daf8c79bf9f050133e34a9912b8248208
Cassiano Montibeller <cassianomonbeller@gmail.com>
2020-06-25 18:26:25 -0300

    Adding tests for Drag and Moment function

--- ADR/Methods/tests/test_FundamentalEquations.py
+++ ADR/Methods/tests/test_FundamentalEquations.py
@@ -4,3 +4,9 @@ import numpy.testing as npt
 
 def test_lift():
     npt.assert_almost_equal(lift(1.25, 14.3, 2.7, 0.9), 310.569, decimal=3)
+
+def test_drag():
+    npt.assert_almost_equal(drag(1.25, 14.3, 2.7, 0.5), 172.538, decimal=3)
+
+def test_moment():
+    npt.assert_almost_equal(moment(1.25, 14.3, 2.7, 0.6), 207.046, decimal=3)
