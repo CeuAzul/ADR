817058b77f69deaf1ffa1873768c43f35e44ebb2
joaofoes <firtafoes@gmail.com>
2020-06-26 20:35:00 -0300

    Changed variables names and changed parameters for better data visualization

--- ADR/Components/Propulsion/test/test_Motor.py
+++ ADR/Components/Propulsion/test/test_Motor.py
@@ -3,5 +3,6 @@ import numpy.testing as npt
 
 
 def test_Motor():
-    mydict = {"static_thrust": 14, "linear_decay_coefficient": 2}
-    npt.assert_almost_equal(Motor(mydict).thrust(35), -56, decimal=0)
+    motor_parameters = {"static_thrust": 14, "linear_decay_coefficient": 2}
+    motor = Motor(motor_parameters)
+    npt.assert_almost_equal(motor.thrust(5), 4, decimal=0)
