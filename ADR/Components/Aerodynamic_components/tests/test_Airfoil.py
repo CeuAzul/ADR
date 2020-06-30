2361ebbd26681b5ca6d8d298382668bfbf1ccf4b
joaofoes <firtafoes@gmail.com>
2020-06-30 12:58:27 -0300

    Add pytest class syntax and changed variable names

--- ADR/Components/Aerodynamic_components/tests/test_Airfoil.py
+++ ADR/Components/Aerodynamic_components/tests/test_Airfoil.py
@@ -1,20 +1,22 @@
-from ADR.Components.Aerodynamic_components.Airfoil import *
+from ADR.Components.Aerodynamic_components.Airfoil import Airfoil
 import numpy.testing as npt
+import pytest
 
 
-def test_get_Cl():
-    aircraft_airfoil_data = {"airfoil_name": "s1223"}
-    aircraft_Coefficients = Airfoil(aircraft_airfoil_data)
-    npt.assert_almost_equal(aircraft_Coefficients.get_Cl(3), 1.5503, decimal=4)
+@pytest.fixture
+def airfoil():
+    airfoil_data = {"airfoil_name": "s1223"}
+    airfoil = Airfoil(airfoil_data)
+    return airfoil
 
 
-def test_get_Cd():
-    aircraft_airfoil_data = {"airfoil_name": "s1223"}
-    aircraft_Coefficients = Airfoil(aircraft_airfoil_data)
-    npt.assert_almost_equal(aircraft_Coefficients.get_Cd(3), 0.0214, decimal=4)
+def test_get_Cl(airfoil):
+    npt.assert_almost_equal(airfoil.get_Cl(3), 1.5503, decimal=4)
 
 
-def test_get_Cm():
-    aircraft_airfoil_data = {"airfoil_name": "s1223"}
-    aircraft_Coefficients = Airfoil(aircraft_airfoil_data)
-    npt.assert_almost_equal(aircraft_Coefficients.get_Cm(3), -0.2761, decimal=4)
+def test_get_Cd(airfoil):
+    npt.assert_almost_equal(airfoil.get_Cd(3), 0.0214, decimal=4)
+
+
+def test_get_Cm(airfoil):
+    npt.assert_almost_equal(airfoil.get_Cm(3), -0.2761, decimal=4)
