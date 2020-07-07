0f21c763884430d471d06527553e7fc3357b3f95
joaofoes <firtafoes@gmail.com>
2020-07-07 18:11:30 -0300

    Finished moment_on_CG test file

--- ADR/Components/Aerodynamic_components/tests/test_Moment_on_CG.py
+++ ADR/Components/Aerodynamic_components/tests/test_Moment_on_CG.py
@@ -27,35 +27,14 @@ def test_moment_on_CG():
 
     aerodynamic_surface = Aerodynamic_surface(aerodynamic_surface_parameters)
 
-    cg_parameters = {"x": 0.15, "z": 0.2}
+    plane_alpha = 3.0
+    aerodynamic_surface.update_alpha(plane_alpha)
+
+    cg_parameters = {"x": 0.15, "y": 0.7, "z": 0.2}
     cg = CG(cg_parameters)
 
     npt.assert_almost_equal(
-        aerodynamic_surface.moment_on_CG(aerodynamic_surface_parameters, cg, 3.1), 1
+        aerodynamic_surface.moment_on_CG(aerodynamic_surface, cg, plane_alpha),
+        -0.37,
+        decimal=2,
     )
-
-
-aerodynamic_surface_parameters = {
-    "airfoil_clmax": 2.5,
-    "airfoil1_name": "s1223",
-    "airfoil2_name": "s1223",
-    "airfoil3_name": "s1223",
-    "span1": 2,
-    "span2": 1.5,
-    "chord1": 0.40,
-    "chord2": 0.30,
-    "chord3": 0.35,
-    "twist1": 0,
-    "twist2": 0,
-    "twist3": 0,
-    "incidence": 0,
-    "x": 1,
-    "z": 1,
-}
-
-aerodynamic_surface = Aerodynamic_surface(aerodynamic_surface_parameters)
-
-cg_parameters = {"x": 0.15, "y": 0.7, "z": 0.2}
-cg = CG(cg_parameters)
-
-CM_surface_alpha3 = aerodynamic_surface.moment_on_CG(aerodynamic_surface, cg, 3.0)
