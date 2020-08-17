6f4b797f171973043fb7785da3914ffe40b6701d
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2020-08-17 14:27:42 -0300

    Add fixture, instantiation test and state test for AttachedComponent class

--- tests/Components/test_AttachedComponent.py
+++ tests/Components/test_AttachedComponent.py
@@ -1,5 +1,27 @@
 from adr.Components import AttachedComponent
+from vec import Vector2
+import math
+import pytest
 
 
-def test_instantiation():
-    pass
+@pytest.fixture
+def attached_component():
+    attached_component = AttachedComponent(
+        name='attached_component',
+        type='generic_attached_component',
+        mass=1.4,
+        relative_position=Vector2(-0.4, 0.1),
+        relative_angle=math.radians(9)
+    )
+    return attached_component
+
+
+def test_instantiation(attached_component):
+    assert(attached_component.relative_position.x == -0.4)
+    assert(attached_component.relative_position.y == 0.1)
+    assert(attached_component.relative_angle == math.radians(9))
+
+
+def test_states(attached_component):
+    attached_component.actuation_angle = math.radians(3)
+    assert(attached_component.actuation_angle == math.radians(3))
