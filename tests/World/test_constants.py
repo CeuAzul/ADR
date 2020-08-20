97614e675aa706ded4b35a56d41a4869af4bcf7c
joaofoes <firtafoes@gmail.com>
2020-08-20 11:12:14 -0300

    Add test for constants

--- tests/World/test_constants.py
+++ tests/World/test_constants.py
@@ -1,5 +1,11 @@
 from adr.World import constants
+import numpy.testing as npt
 
 
-def test_air_molar_mass():
-    pass
+def test_fixed_constants():
+    assert constants.air_molar_mass == 0.02896
+    assert constants.gravitational_acceleration == 9.80665
+
+
+def test_air_gas_constant():
+    npt.assert_almost_equal(constants.air_gas_constant, 287.101, decimal=3)
