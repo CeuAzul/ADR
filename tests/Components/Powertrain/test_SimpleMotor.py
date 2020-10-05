65156993647250424ee4d7ac3bf49f8f549bc19f
joaofoes <firtafoes@gmail.com>
2020-10-04 22:22:58 -0300

    Minor changes to test_SimpleMotor

--- tests/Components/Powertrain/test_SimpleMotor.py
+++ tests/Components/Powertrain/test_SimpleMotor.py
@@ -2,7 +2,7 @@ import attr
 from vec import Vector2
 from adr.World import Ambient
 from adr.Components import FreeBody
-from adr.Components.Powertrain import Motor, SimpleMotor
+from adr.Components.Powertrain import SimpleMotor
 from adr.Methods.Powertrain.thrust_equations import get_axial_thrust_from_linear_model
 import numpy.testing as npt
 import math
@@ -16,7 +16,7 @@ def motor():
        relative_position=Vector2(-0.4, 0.1),
        relative_angle=math.radians(0),
        static_thrust = 78,
-       linear_coefficient = 1,
+       linear_coefficient = -1,
        distance_origin_to_propeller = 0.3
     )
     return motor
@@ -48,7 +48,8 @@ def test_get_thrust(motor, plane):
     x,y = motor.get_thrust()
 
     check_thrust_center = Vector2(0.3, 0)
-    check_thrust = Vector2(82.26940810871174, 0)
+    check_thrust = Vector2(82.269378, 0)
 
     npt.assert_array_almost_equal(x, check_thrust)
     npt.assert_array_almost_equal(y, check_thrust_center)
+    
